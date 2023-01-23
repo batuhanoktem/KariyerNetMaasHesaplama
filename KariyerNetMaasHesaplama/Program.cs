01792bbe7e32653bd5adcfb97797bc61fd624f75
Ergun Batuhan Öktem <batuhanoktem@gmail.com>
2023-01-23 04:15:48 +0300

    Add project files.

---
+++ KariyerNetMaasHesaplama/Program.cs
@@ -0,0 +1,74 @@
+﻿using HtmlAgilityPack;
+using SpreadCheetah;
+using SpreadCheetah.Styling;
+using SpreadCheetah.Worksheets;
+using System.Globalization;
+using System.Web;
+
+var outputFile = "Maaslar.xlsx";
+if (File.Exists(outputFile))
+    File.Delete(outputFile);
+
+var document = new HtmlWeb().Load("https://www.kariyer.net/pozisyonlar");
+
+using var stream = File.Create(outputFile);
+using var spreadsheet = await Spreadsheet.CreateNewAsync(stream);
+var worksheetOptions = new WorksheetOptions();
+worksheetOptions.Column(1).Width = 50;
+worksheetOptions.Column(2).Width = 18;
+worksheetOptions.Column(3).Width = 18;
+worksheetOptions.Column(4).Width = 18;
+await spreadsheet.StartWorksheetAsync("Maaşlar", worksheetOptions);
+
+var headerStyle = new Style();
+headerStyle.Font.Bold = true;
+var headerStyleId = spreadsheet.AddStyle(headerStyle);
+
+var cells = new[]
+{
+    new Cell("POZİSYON", headerStyleId),
+    new Cell("ORTALAMA MAAŞ", headerStyleId),
+    new Cell("EN DÜŞÜK MAAŞ", headerStyleId),
+    new Cell("EN YÜKSEK MAAŞ", headerStyleId)
+};
+await spreadsheet.AddRowAsync(cells);
+
+var positionNodes = document.DocumentNode.SelectNodes("//div[@class='pgl-positions']//a");
+for (int i = 0; i < positionNodes.Count; i++)
+{
+    var positionNode = positionNodes[i];
+    document = new HtmlWeb().Load($"https://www.kariyer.net{positionNode.GetAttributeValue("href", null)}");
+
+    try
+    {
+        var position = HttpUtility.HtmlDecode(document.DocumentNode.SelectSingleNode("//h1").InnerText);
+        var averageSalary = document.DocumentNode.SelectSingleNode("//div[@class='pg-salary-box']/div[@class='pg-salary-box-value']")?.InnerText;
+
+        if (averageSalary == null)
+            continue;
+
+        var minimumSalary = document.DocumentNode.SelectSingleNode("//div[@class='pg-salary-box double']/div[@class='left']/div[@class='pg-salary-box-value']").InnerText;
+        var maximumSalary = document.DocumentNode.SelectSingleNode("//div[@class='pg-salary-box double']/div[@class='right']/div[@class='pg-salary-box-value']").InnerText;
+
+        Console.WriteLine($"POZİSYON: {position}");
+        Console.WriteLine($"ORTALAMA MAAŞ: {averageSalary}");
+        Console.WriteLine($"EN DÜŞÜK MAAŞ: {minimumSalary}");
+        Console.WriteLine($"EN YÜKSEK MAAŞ: {maximumSalary}");
+
+        cells = new[]
+        {
+            new Cell(position),
+            new Cell(Convert.ToDecimal(averageSalary.Replace(".", ""), CultureInfo.InvariantCulture)),
+            new Cell(Convert.ToDecimal(minimumSalary.Replace(".", ""), CultureInfo.InvariantCulture)),
+            new Cell(Convert.ToDecimal(maximumSalary.Replace(".", ""), CultureInfo.InvariantCulture))
+        };
+        await spreadsheet.AddRowAsync(cells);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+        if (e.InnerException != null)
+            Console.WriteLine(e.InnerException.Message);
+    }
+}
+await spreadsheet.FinishAsync();
---
+++ README.md
@@ -0,0 +1,13 @@
+﻿# Kariyer.net Maaş hesaplama
+
+## Bağış
+  - Yapılan bağışlar projenin gelişmesine katkıda bulunacaktır.
+  - Bitcoin: 1FHFU14DRWWHaJyq8fK1coKR57KqQJg2ZH
+
+## Hakkında
+- https://www.kariyer.net/pozisyonlar sayfasında bulunan pozisyonların maaş bilgisini Excel dosyası halinde sunmaya yarar.
+- Örnek maaş bilgisine [Maaslar.xlsx](Maaslar.xlsx) dosyasından ulaşabilirsiniz.
+
+## Kullanılan Kütüphaneler:
+  - [Html Agility Pack (HAP)](https://github.com/zzzprojects/html-agility-pack "Html Agility Pack (HAP)")
+  - [SpreadCheetah](https://github.com/sveinungf/spreadcheetah "SpreadCheetah")
